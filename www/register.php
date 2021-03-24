58e5f551f901febd7bdeb981eba57f9627b022d9
LaingMASTER7 <nassim.bellik32@gmail.com>
2021-03-24 17:52:19 +0100

    incription

--- www/register.php
+++ www/register.php
@@ -7,6 +7,21 @@ require_once dirname(__FILE__).'/../../config/config.php';
     exit ('Erreur while connecting to database: '.$e->getMessage());
   }
 
-echo "or noir 5 dans les back !!!"
+echo 'inscription<br>
+<form action="register.php" method="post">
+<input type="email" name="email" placeholder="Veulliez entrez votre mail" required="yes"/><br>
+<input type="text" name="pseudo" placeholder="Entrez votre pseudo" required="yes"/><br>
+<input type="password" name="mdp" placeholer="Mot de passe" required="yes"/><br>
+<input type="password" name="vmdp" placeholer="Confirmez votre mot de passe" required="yes"/><br>
+<input type="number" name="role" placeholer="rôle" required="yes"/><br>
+<select name="role" id="role">
+<option value="0">Elève</option>
+<option value="1">Modérateur</option>
+<option value="2">Professeur</option>
+</select><br>
+<input type="number" name="annee" placeholer="année" required="yes"/><br>
+<input type="number" name="majeure" placeholer="majeure" required="yes"/><br>
+<input type="submit" value="S\'incrire maintenant">
+</form>'
 
 ?>
