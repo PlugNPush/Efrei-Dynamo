f83592a4c338a40c28c97229dde2041b8d3d8e0f
LaingMASTER7 <nassim.bellik32@gmail.com>
2021-03-24 16:51:57 +0100

    kaaris

---
+++ www/register.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__).'/../../config/config.php';
+  try {
+    $bdd = new PDO('mysql:host='.getDBHost().';dbname=efreidynamo', getDBUsername(), getDBPassword(), array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
+  } catch(Exception $e) {
+    exit ('Erreur while connecting to database: '.$e->getMessage());
+  }
+
+echo "or noir 5 dans les back !!!"
+
+?>
