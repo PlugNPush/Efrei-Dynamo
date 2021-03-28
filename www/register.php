1c9ee009c2cd631cf834738ae4e1b1994f38a9e7
Thiegox <72384921+Thiegox@users.noreply.github.com>
2021-03-28 18:18:06 +0200

    réparation ultime

--- www/register.php
+++ www/register.php
@@ -1,14 +1,11 @@
 <?php
-
 require_once dirname(__FILE__).'/../../config/config.php';
   try {
     $bdd = new PDO('mysql:host='.getDBHost().';dbname=efreidynamo', getDBUsername(), getDBPassword(), array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
   } catch(Exception $e) {
     exit ('Erreur while connecting to database: '.$e->getMessage());
   }
-
 if(!isset($_POST['mdp']) AND !isset($_POST['vmdp'])){
-
   echo '<!DOCTYPE html>
   <html>
       <head>
@@ -20,6 +17,8 @@ if(!isset($_POST['mdp']) AND !isset($_POST['vmdp'])){
   <form action="register.php" method="post">
   <input type="email" name="email" placeholder="Veulliez entrez votre mail" required="yes"/><br>
   <input type="text" name="pseudo" placeholder="Entrez votre pseudo" required="yes"/><br>
+  <input type="password" name="mdp" placeholer="Mot de passe" required="yes"/><br>
+  <input type="password" name="vmdp" placeholer="Confirmez votre mot de passe" required="yes"/><br>
   <input type="password" name="mdp" placeholder="Mot de passe" required="yes"/><br>
   <input type="password" name="vmdp" placeholder="Confirmez votre mot de passe" required="yes"/><br>
   <select name="role" id="role">
@@ -34,6 +33,7 @@ if(!isset($_POST['mdp']) AND !isset($_POST['vmdp'])){
   <option value="4">M1</option>
   <option value="5">M2</option>
   </select><br>
+  <input type="number" name="majeure" placeholer="majeure" required="yes"/><br>
   <input type="number" name="majeure" placeholder="Majeure" required="yes"/><br>
   <input type="submit" value="S\'incrire maintenant">
   </form>
