21756782fdad764d84a9e409d7e74ade45058d83
LaingMASTER7 <nassim.bellik32@gmail.com>
2021-04-28 23:14:10 +0200

    le clavier est kosovar

---
+++ www/newresponse.php
@@ -0,0 +1,52 @@
+<?php
+require_once dirname(__FILE__).'/../../config/config.php';
+  try {
+    $bdd = new PDO('mysql:host='.getDBHost().';dbname=efreidynamo', getDBUsername(), getDBPassword(), array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
+  } catch(Exception $e) {
+    exit ('Erreur while connecting to database: '.$e->getMessage());
+  }
+  session_start();
+  if (!isset($_GET['question'])){
+    echo '<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>Erreur</title>
+        </head>
+        <body>
+    Il semblerait que vous ne répondiez à personne...<br>
+    </body>';
+
+  } else if(!isset($_GET['question']) AND !isset($_POST['contenu'])){
+  echo '<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8"/>
+          <title>Nouvelle réponse</title>
+      </head>
+      <body>
+  Nouvelle réponse<br>
+  <form action="newresponse.php?question=',$_GET['question'],'" method="post">
+  <textarea name="contenu" rows="15" placeholder="Expliquez votre réponse" required="yes"></textarea><br>
+  <input type="submit" value="Envoyez la réponse">
+  </form>
+  </body>';
+  } else {
+  $req=$bdd->prepare('INSERT INTO questions(auteur, titre, contenu, matiere, date) VALUES(:auteur, :titre, :contenu, :matiere, :date)');
+  if (isset($_SESSION['id'])) {
+    $date = date('Y-m-d H:i:s');
+    $req->execute(array(
+      'auteur'=> $_SESSION['id'],
+      'titre'=> $_POST['titre'],
+      'contenu'=> $_POST['contenu'],
+      'matiere'=> $_POST['matiere'],
+      'date'=> $date
+    ));
+    header( "refresh:5;url=index.php" );
+    echo 'Votre question a bien été envoyée !';
+  }else{
+    header( "refresh:5;url=login.php" );
+    echo 'Votre session a expiré.';
+  }
+}
+?>
