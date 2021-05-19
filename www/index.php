fac62b5ff833ea0143455b268cb9d0dbdc3e74c5
LaingMASTER7 <nassim.bellik32@gmail.com>
2021-05-19 18:55:58 +0200

    .

--- www/index.php
+++ www/index.php
@@ -199,7 +199,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['validation']) && $_SESSION['valid
             <div class="card my-4">
               <h5 class="card-header">Récapitulatif</h5>
               <div class="card-body">
-                Vous avez posé ', $questions['COUNT(*)'],' questions, et vous avez répondu à ', $reponses['COUNT(*)'],' questions sur Efrei Dynamo. ', $repondues['COUNT(*)'],' questions sont en attente de réponses/validation.
+                Vous avez posé ', $questions['COUNT(*)'],' questions, et vous avez répondu à ', $reponses['COUNT(*)'],' questions sur Efrei Dynamo. ', $repondues['COUNT(*)'],' questions sont en attente de validation.
               </div>
             </div>
 
