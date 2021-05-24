eb46a73d3c39ec41f72433dbd187c157e6277e0b
PlugN <michael.nass@free.fr>
2021-05-25 00:47:40 +0200

    ok

--- www/account.php
+++ www/account.php
@@ -309,7 +309,7 @@ if (!isset($_POST['id'])) {
                 </div><br>
                 <div class="form-check">
                   <input class="form-check-input" type="checkbox" id="supcontenu" name="supcontenu" class="form-control" required>
-                  <label class="form-check-label" for="supcontenu">Supprimer également le contenu produit par ce compte (questions, réponses...)</label>
+                  <label class="form-check-label" for="supcontenu">Facultatif : je souhaite également supprimer le contenu produit par ce compte (questions, réponses...)</label>
                 </div><br>
                   <button class="btn btn-danger" type="submit">Supprimer ce compte</button>
                 </form>
