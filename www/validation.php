fe6676666f2f50d4138a0c9806414b87e418a444
PlugN <michael.nass@free.fr>
2021-05-25 18:47:23 +0200

    ok

--- www/validation.php
+++ www/validation.php
@@ -62,10 +62,8 @@ if (isset($_SESSION['id'])){
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">Répondre à des questions
-                  <span class="sr-only">(current)</span>
-                </a>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Répondre à des questions</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="newquestion.php">Poser une question</a>
