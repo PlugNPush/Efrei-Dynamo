8679297189e2148e8bb5b703618a4fe608c68a2b
PlugN <michael.nass@free.fr>
2021-05-26 00:28:03 +0200

    test ?

--- www/validation.php
+++ www/validation.php
@@ -44,7 +44,7 @@ if (isset($_SESSION['id'])){
     $newrole->execute(array($_SESSION['id']));
 
     header( "refresh:0;url=validation.php" );
-  } else if (!isset($_POST['email']) && !isset($_GET['token'])){
+  } else if (!isset($_POST['email']) && !isset($_GET['token']) && !isset($_GET['resend']) && !isset($_GET['cancel'])){
     echo '<!DOCTYPE html>
     <html lang="fr">
 
@@ -147,6 +147,12 @@ if (isset($_SESSION['id'])){
               </div>';
             }
 
+            if (isset($_GET['resent'])) {
+              echo '<div class="alert alert-success fade show" role="alert">
+                <strong>Email renvoyé !</strong><br> Votre code d\'authentification vous a été envoyé une nouvelle fois sur votre adresse mail. Pensez à vérifier vos spams.
+              </div>';
+            }
+
             if (isset($_SESSION['validation']) && $_SESSION['validation'] == 1 && $data) {
               echo '<div class="alert alert-success fade show" role="alert">
                 <strong>Félicitations, votre compte Efrei est validé !</strong><br>Vous n\'avez rien à faire, nous avons vérifié votre appartenance à l\'Efrei avec l\'adresse email suivante : <a href="mailto:', $data['email'] ,'">', $data['email'] ,'</a>.
@@ -170,6 +176,8 @@ if (isset($_SESSION['id'])){
                   </small>
                 </div>
                 <button type="submit" class="btn btn-primary">Vérifier l\'authenticité du compte</button>
+                <a href="validation.php?resend=true" class="btn btn-secondary">Renvoyer le code</a>
+                <a href="validation.php?cancel=true" class="btn btn-danger">Annuler la validation</button>
                 </form><br><br>';
             } else {
 
@@ -228,6 +236,65 @@ if (isset($_SESSION['id'])){
 
     </html>
 ';
+} else if (isset($_GET['resend'])){
+
+  $gatherdata = $bdd->prepare('SELECT * FROM validations WHERE user = ?;');
+  $gatherdata->execute(array($_SESSION['id']));
+  $data = $gatherdata->fetch();
+
+  if ($data) {
+    $to = 'motherboardplus@gmail.com'; // $_POST['email']
+    $subject = 'Verification automatique Efrei Dynamo';
+    $message = '
+        <html>
+         <body>
+          <h1>Vérification automatique d\'appartenance à l\'Efrei.</h1>
+          <p>Bonjour ' . $_SESSION['pseudo'] . ', et bienvenue sur Efrei Dynamo. Pour confirmer votre inscription, vous devez prouver votre appartenance à l\'Efrei. Grâce à votre adresse email Efrei, vous êtes éligible à notre solution de validation automatique. Cliquez simplement sur le lien ci-dessous pour terminer l\'activation de votre compte.
+          <p>Adresse email utilisée</p>
+          <h4>' . $data['email'] . '</h4>
+          <p>Certification demandée le</p>
+          <h4>' . $data['date'] . '</h4>
+          <br><br>
+          <a href="https://www.efrei-dynamo.fr/validation.php?token=' . $token . '">Cliquez ici pour activer automatiquement votre compte</a>.
+          <br>
+          <p>En cas de problème, votre code de validation est</p>
+          <h4>' . $data['token'] . '</h4>
+          <br>
+          <p>À très vite !</p><br>
+          <p>- L\'équipe Efrei Dynamo.</p><br><br>
+          <p>P.S.: Ce courriel est automatique, veuillez ne pas y répondre.</p>
+       </body>
+      </html>
+      ';
+
+      // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+      $headers[] = 'MIME-Version: 1.0';
+      $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+      // En-têtes additionnels
+      $headers[] = 'To: ' . $_SESSION['pseudo'] . '<'. $_POST['email'] .'>';
+      $headers[] = 'From: Système de validation Efrei Dynamo <noreply@efrei-dynamo.fr>';
+
+      // Envoi
+      $sent = mail($to, $subject, $message, implode("\r\n", $headers));
+
+      if ($sent) {
+        header( "refresh:0;url=validation.php?resent=true" );
+      } else {
+        header( "refresh:0;url=validation.php?serror=true" );
+      }
+
+  } else {
+
+  }
+
+} else if (isset($_GET['cancel'])){
+
+  $deletetoken = $bdd->prepare('DELETE FROM validations WHERE user = ?');
+  $deletetoken->execute(array($_SESSION['id']));
+
+  header( "refresh:0;url=validation.php" );
+
 } else if (!isset($_GET['token'])){
 
   if ((strpos($_POST['email'], "@efrei.net") !== false AND $_SESSION['role'] == 0) OR (strpos($_POST['email'], "@efrei.fr") !== false AND $_SESSION['role'] == 2)) {
