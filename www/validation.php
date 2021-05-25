22c6f049e49ecc2476ef43174d1b741a719f46b2
PlugN <michael.nass@free.fr>
2021-05-25 23:54:58 +0200

    oops

--- www/validation.php
+++ www/validation.php
@@ -281,7 +281,7 @@ if (isset($_SESSION['id'])){
   $vkey->execute(array($_GET['key']));
   $key = $vkey->fetch();
 
-  if ($key && ((strpos($_POST['email'], "@efrei.net") !== false AND $_SESSION['role'] == 0) OR (strpos($_POST['email'], "@efrei.fr") !== false AND $_SESSION['role'] == 2))) {
+  if ($key && ((strpos($key['email'], "@efrei.net") !== false AND $_SESSION['role'] == 0) OR (strpos($key['email'], "@efrei.fr") !== false AND $_SESSION['role'] == 2))) {
     $validation = $bdd->prepare('UPDATE utilisateurs SET validation = 1 WHERE id = ?;');
     $validation->execute(array($_SESSION['id']));
 
