e687bc6dc1d912770161b774b0e67d414c437ad9
PlugN <michael.nass@free.fr>
2021-05-26 15:38:02 +0200

    test

--- www/index.php
+++ www/index.php
@@ -154,7 +154,7 @@ if (isset($_SESSION['id'])){
                   </div>
                   <div class="card-footer text-muted">
                     Publié le ', $temp_question['date'],' par
-                    <a href="account.php?id=', $auteur['id'] ,'">', $auteur['pseudo'],'</a><br>', $cours['nom'] . ($cours['semestre'] != 0) ? (' (Semestre ' . $cours['semestre'] . ')') : (""), '
+                    <a href="account.php?id=', $auteur['id'] ,'">', $auteur['pseudo'],'</a><br>', $temp_question['matiere'] . $cours['nom'] . ($cours['semestre'] != 0) ? (' (Semestre ' . $cours['semestre'] . ')') : (""), '
                   </div>
                 </div>';
               }
