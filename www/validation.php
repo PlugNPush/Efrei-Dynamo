2adde7fd7298b7005c20a62467068606748f3ba0
PlugN <michael.nass@free.fr>
2021-05-26 23:01:10 +0200

    test

--- www/validation.php
+++ www/validation.php
@@ -199,9 +199,17 @@ if (isset($_SESSION['id'])){
 
               if ($_SESSION['role'] == 0 || $_SESSION['role'] == 2) {
                 echo '<div class="alert alert-warning fade show" role="alert">
-                  <strong>Bonjour ', $_SESSION['pseudo'], ' !</strong><br> Vous devez confirmer votre statut d\'Efreien pour accéder au site.<br>Veuillez suivre les instructions ci-dessous pour procéder à la validation.
-                </div>
+                  <strong>Bonjour ', $_SESSION['pseudo'], ' !</strong><br> Vous devez confirmer votre statut d\'Efreien pour accéder au site.<br>Veuillez suivre les instructions ci-dessous pour procéder à la validation.<br>
+                </div>';
 
+                if ($_SESSION['role'] == 2) {
+                  echo '<div class="alert alert-warning fade show" role="alert">
+                    <strong>Pas d\'adresse email en @efrei.fr ?</strong> Contactez un modérateur pour manuellement valider votre compte, ou rétrogradez vers un profil étudiant pour valider votre immédiatement.
+                  </div>
+                  <a href="validation.php?downgrade=true" class="btn btn-warning btn-lg btn-block">Rétrograder vers un profil étudiant</a><br>';
+                }
+
+                echo '
                 <form action="validation.php" method="post">
                   <div class="form-group">
                     <label for="email">Confirmez votre adresse mail Efrei</label>
