c6451632620704157ac1ee159dca769c1cc9d7ea
PlugN <michael.nass@free.fr>
2021-05-26 22:46:46 +0200

    test

--- www/account.php
+++ www/account.php
@@ -146,7 +146,7 @@ if (!isset($_GET['edit']) && !isset($_GET['pdelete'])) {
               }
               echo '
             </div><br>';
-          } else if ($_SESSION['role'] >= 1 && $_SESSION['id'] != $data['id']) {
+          } else if ($_SESSION['role'] >= 1 && $_SESSION['id'] != $data['id'] && (!isset($data['ban']) || $data['ban'] < $date)) {
             echo '
             <div class="alert alert-success fade show" role="alert">
               <strong>Ce compte est en règle !</strong> Nous vous affichons ce message car vous êtes un modérateur d\'Efrei Dynamo. Ce compte n\'a actuellement aucune restriction. Si ce compte s\'est mal comporté, vous pouvez le bannir pour une durée d\'un mois.<br><a class = "btn btn-danger" href = "logout.php">Bannir ce compte</a>
