58175bf0ed24d9fd0c70e21fbe33fa2529bff97d
PlugN <michael.nass@free.fr>
2021-05-27 23:18:11 +0200

    test

--- www/index.php
+++ www/index.php
@@ -59,6 +59,8 @@ if (isset($_SESSION['id'])){
       <!-- Custom styles for this template -->
       <link href="css/blog-home.css" rel="stylesheet">
 
+      <link href="css/custom.css" rel="stylesheet">
+
     </head>
 
     <body>
